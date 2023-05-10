4596fabbbcbecdf24811252cbeb84646660e8057
Umbracon <48327272+Umbracon@users.noreply.github.com>
2023-05-10 11:53:20 +0200

    Dodaj pliki projektów.

---
+++ Infrastructure/Mappers/QuizMappers.cs
@@ -0,0 +1,37 @@
+﻿using ApplicationCore.Models;
+using Infrastructure.EF.Entities;
+
+namespace Infrastructure.Mappers
+{
+    public static class QuizMappers
+    {
+        public static QuizItem FromEntityToQuizItem(QuizItemEntity entity)
+        {
+            return new QuizItem(
+                entity.Id,
+                entity.Question,
+                entity.IncorrectAnswers.Select(e => e.Answer).ToList(),
+                entity.CorrectAnswer);
+        }
+
+        public static Quiz FromEntityToQuiz(QuizEntity entity)
+        {
+            return new Quiz(
+                entity.Id,
+                entity.Items.Select(FromEntityToQuizItem).ToList(),
+                entity.Title
+                );
+
+        }
+
+        public static QuizItemUserAnswer FromEntityToQuizItemUserAnswer(QuizItemUserAnswerEntity entity)
+        {
+            return new QuizItemUserAnswer(
+                              quizItem: FromEntityToQuizItem(entity.QuizItem),
+                              userId: entity.UserId,
+                              quizId: entity.QuizId,
+                              answer: entity.UserAnswer
+                                );
+        }
+    }
+}
---
+++ QuizUnitTest/QuizApiTest.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using WebAPI.Controllers;
+
+namespace QuizUnitTest
+{
+    public class QuizApiTest
+    {
+        [Fact]
+        public async void GetShouldReturnTwoQuizzes()
+        {
+            //Arrange
+            await using var application = new WebApplicationFactory<Program>();
+            using var client = application.CreateClient();
+
+            //Act
+            var result = await client.GetFromJsonAsync<List<QuizDto>>("/api/v1/quizzes");
+
+            //Assert
+            Assert.Equal(2, result.Count);
+        }
+        [Fact]
+        public async void GetShouldReturnOkStatus()
+        {
+            //Arrange
+            await using var application = new WebApplicationFactory<Program>();
+            using var client = application.CreateClient();
+
+            //Act
+            var result = await client.GetAsync("/api/v1/quizzes");
+
+            //Assert
+            Assert.Equal(HttpStatusCode.OK, result.StatusCode);
+            Assert.Contains("application/json", result.Content.Headers.GetValues("Content-Type").First());
+        }
+    }
+}
